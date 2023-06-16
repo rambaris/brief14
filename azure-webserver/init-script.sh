78bc31b83a81ffd9481bb2e54d43c3f6442d6167
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-06-16 11:55:11 +0200

    Update init-script.sh

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -4,5 +4,4 @@ sudo apt install curl
 #Installing Docker
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-#Creating container
-sudo docker run -d -p 8080:80 --name docker-nginx fabiomp/docker-nginx:latest
+
