e4d38d3d98f4a2196e70c8ae90b727b839e0c89c
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-06-16 15:07:33 +0200

    Update init-script.sh

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -5,3 +5,6 @@ sudo apt install curl
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
+#Creating container
+sudo docker pull rambaris/myapppython
+sudo docker run -d --name webserver -p 80:80 #Creating container
