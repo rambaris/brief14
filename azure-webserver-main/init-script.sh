230c8a46ae82d0dfb917ef50266869dc4a689ce0
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-06-16 11:53:45 +0200

    Delete azure-webserver-main directory

--- azure-webserver-main/init-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Installing Docker
-
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install curl
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
